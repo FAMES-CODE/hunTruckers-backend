1befebc08837c448c132f35fe95ca3d31b8279b0
Amine Ferkani <ferkaniamine08@gmail.com>
2024-01-23 10:51:52 +0100

    modified:   controllers/playerController.js Deleted streamers controller 	modified:   routes/playerRoute.js Deleted streamers controller

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -18,27 +18,6 @@ async function getUserData(req, res) {
     });
 }
 
-/* Get list of streamer playing ETS2*/
-
-async function getStreamer(req, res) {
-  let game = req.params.game;
-  let url = "https://api.truckyapp.com/v2/streams/twitch/ets2";
-  await axios
-    .get(url)
-    .then((response) => {
-      var data = response.data;
-      res.send({
-        data,
-      });
-    })
-    .catch((err) => {
-      res.status(404).send({
-        message: err.message,
-      });
-    });
-}
-
 module.exports = {
   getUserData,
-  getStreamer,
 };
--- routes/playerRoute.js
+++ routes/playerRoute.js
@@ -4,7 +4,5 @@ const router = express.Router();
 const playerController = require("../controllers/playerController.js");
 
 router.get("/:pid", (req, res) => playerController.getUserData(req, res));
-router.get("/streamers", (req, res) => playerController.getUserData(req, res));
-
 
 module.exports = router;
